ab5464117cf423d2f804a6d4bb519fd49af1c860
Morgan Sell <MorganPSell@gmail.com>
2026-06-11 20:21:35 -0700

    create geocoding service to be used by the Neighborhood Scout (#23)

---
+++ scene_scout/geocoding_config.py
@@ -0,0 +1,8 @@
+"""Geocoding policy constants for SceneScout."""
+
+NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org"
+OVERPASS_API_URL = "https://overpass-api.de/api/interpreter"
+NOMINATIM_USER_AGENT = "SceneScout/0.1.0 (local event discovery)"
+GEOCODING_RATE_LIMIT_SECONDS = 1.0
+DEFAULT_POI_RADIUS_M = 1000
+GEOCODING_HTTP_TIMEOUT_SECONDS = 15
